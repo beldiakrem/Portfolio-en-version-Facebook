e0f2b93eecd7980db00c68be53cf3267239a27a4
akrem <baldiakrem@gmail.com>
2021-03-21 12:45:21 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Components/header'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/header.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Nav from 'react-bootstrap/Nav';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown'
+import logo from '../Assests/logo.png'
+import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+import PermPhoneMsgIcon from '@material-ui/icons/PermPhoneMsg';
+import ArchiveIcon from '@material-ui/icons/Archive';
+import IconButton from '@material-ui/core/IconButton';
+import { pink } from '@material-ui/core/colors';
+import Tooltip from '@material-ui/core/Tooltip';
+import Avatar from '@material-ui/core/Avatar'; 
+import Me from '../Assests/me.jpg'
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+        margin: '0px 5px',
+      },
+    },
+    gris: {
+      color: theme.palette.getContrastText(pink[500]),
+      backgroundColor: '#3A3B3C',
+      "&:hover":
+        {
+          
+        backgroundColor: '#696969',
+       }
+    },
+   
+    
+  }));
+ 
+ 
+  export default function  Header () {
+    const classes = useStyles();
+    return (
+        <div className="header" >
+            
+            <Navbar >
+             <img id="logo" src={logo} />
+               <Form inline>
+                   <FormControl type="text" placeholder="&#61442;  Rechercher sur Facebook" className="mr-sm-2" />
+               </Form>
+                 <Navbar.Brand id="navlink" href="#home">Navbar</Navbar.Brand>
+                     <Nav className="mr-auto">
+                        <Nav.Link  id="navlink" href="#home">Home</Nav.Link>
+                        <Nav.Link  id="navlink" href="#features">Features</Nav.Link>
+                        <Nav.Link  id="navlink" href="#pricing">Pricing</Nav.Link>
+                     </Nav>
+                     <div className={classes.root}>
+                       
+                          <Avatar  src={Me} />
+                        
+                           <IconButton className={classes.gris}>
+                           <Tooltip title="Add" aria-label="add">
+                        
+                            <AddIcon  />
+                            
+                      </Tooltip>
+                           </IconButton>
+                        
+                           <IconButton className={classes.gris}>
+                           <Tooltip title="Contact" aria-label="contact">
+                            <PermPhoneMsgIcon  />
+                            </Tooltip>
+                           </IconButton>
+                           <IconButton className={classes.gris}>
+                           <Tooltip title="Projects" aria-label="projects">
+                            <ArchiveIcon  />
+                            </Tooltip>
+                           </IconButton>
+                     </div>
+                <ButtonGroup vertical>  
+                     <DropdownButton as={ButtonGroup} title="" id="bg-vertical-dropdown-1" >
+                         <Dropdown.Item id="dropdowncolor" eventKey="1">Dropdown link</Dropdown.Item>
+                         <Dropdown.Item id="dropdowncolor" eventKey="2">Dropdown link</Dropdown.Item>
+                     </DropdownButton>
+                </ButtonGroup>
+             </Navbar>
+            
+        </div>
+    )
+}
+ 
